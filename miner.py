a2076c58afdac55f1dbb57601b3ab07735a64259
pbegle <peterbegle@gmail.com>
2021-06-01 21:47:09 -0700

    Implemented functions to check if miner is on/off and toggle it on/off

--- miner.py
+++ miner.py
@@ -1,10 +1,26 @@
 """
 Functions for toggling the miner on/off
 """
+import subprocess
 
-def toggle_miner_on_off():
-    raise NotImplemented('toggle_miner_on_off not yet implemented')
+import psutil
 
+PROCESS_NAME = 't-rex.exe'
+BAT_FILEPATH = 'ETH-ethermine.bat'
 
 def miner_is_on() -> bool:
-    return True
+    if PROCESS_NAME in (p.name() for p in psutil.process_iter()):
+        return True
+    
+    return False
+
+
+def toggle_miner_on_off():
+    if miner_is_on():
+        # Turn miner off
+        for proc in psutil.process_iter():
+            if proc.name() == PROCESS_NAME:
+                proc.kill()
+    else:
+        # Turn miner on if not running
+        subprocess.call([BAT_FILEPATH])
